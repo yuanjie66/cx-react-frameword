eddf9aea98bd18ea6adb0a0e76c1b8db7de865b7
yuanjie <zhuyuanjie@druid.com>
2024-09-06 14:47:55 +0800

    update: add build config file

--- src/page1/index.tsx
+++ src/page1/index.tsx
@@ -1,12 +1,7 @@
-import {Button} from 'antd';
 import React from 'react';
 
 const Page1: React.FC = (props) => {
-	return (
-		<div>
-			<Button>qwdqwd</Button>
-		</div>
-	);
+	return <div>Page1</div>;
 };
 
 export default Page1;
